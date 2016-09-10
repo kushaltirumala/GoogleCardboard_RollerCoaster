eddc837824e758456f2458f0b7b0e9b8d039ea23
kNelakonda <kavi.nelakonda@gmail.com>
2016-09-09 23:54:50 -0700

    jeffrey tao #5

---
+++ Assets/StartImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartImage : MonoBehaviour {
+
+	public GameObject image;
+	public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("asdf");
+
+	}
+
+}
